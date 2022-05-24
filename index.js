d3b2e05f32d8baae1432615b8d34002f1f6e81da
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-05-24 23:12:30 +0600

    add order done

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwubk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -17,6 +16,7 @@ const run=async()=>{
             await client.connect();
             const productCollection = client.db('assigment12').collection('products')
             const reviewsCollection = client.db('assigment12').collection('reviews')
+            const orderCollection = client.db('assigment12').collection('order')
 
             app.get('/products',async (req, res) => {
                 const quary = {}
@@ -33,6 +33,17 @@ const run=async()=>{
             app.get('/reviews',async (req, res) => {
                 const quary = {}
                 const result = await reviewsCollection.find(quary).toArray()
+                res.send(result)
+            })
+            app.post('/order', async (req, res) => {
+                const order = req.body;
+                console.log(order.productname)
+                const quary = { productname:order.productname}
+                const exists = await orderCollection.findOne(quary)
+                if (exists) {
+                    return res.send({success:false,product:exists})
+                }
+                const result = await orderCollection.insertOne(order)
                 res.send(result)
             })
 
