3d301555c8df9bdd02c46efa604ed9a02341e0ff
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-05-24 15:17:49 +0600

    git add .

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cors())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwubk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run=async()=>{
+        try{
+            await client.connect();
+            const productCollection = client.db('assigment12').collection('products')
+            const reviewsCollection = client.db('assigment12').collection('reviews')
+
+            app.get('/products',async (req, res) => {
+                const quary = {}
+                const result = await productCollection.find(quary).toArray()
+                res.send(result)
+            })
+            app.get('/reviews',async (req, res) => {
+                const quary = {}
+                const result = await reviewsCollection.find(quary).toArray()
+                res.send(result)
+            })
+
+        }
+        finally{
+            
+        }
+}
+run().catch(console.dir)
+
+app.get('/',(req,res)=>{
+    res.send('Assignment 12')
+})
+app.listen(port)
