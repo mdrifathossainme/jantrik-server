f330f8492bdf756485a11626502f20045cd5ba10
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-05-25 12:24:54 +0600

    jtw Token

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
+
+const verifyJTW = (req, res, nest) => {
+    const authHeader = req.headers.authonrization;
+    if (!authHeader) {
+        return res.status(401).send({message:"Unauthorizes Access"})
+    }
+   const token=authHeader.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded)=>{
+        if(err){
+            return res.status(403).sen({message:"Forbidden access"})
+        }
+        req.decoded=decoded;
+        next()
+
+    })
+    
+}
+
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwubk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -17,27 +38,28 @@ const run=async()=>{
             const productCollection = client.db('assigment12').collection('products')
             const reviewsCollection = client.db('assigment12').collection('reviews')
             const orderCollection = client.db('assigment12').collection('order')
+            const userCollection = client.db('assigment12').collection('user')
 
-            app.get('/products',async (req, res) => {
+            app.get('/products',verifyJTW,async (req, res) => {
                 const quary = {}
                 const result = await productCollection.find(quary).toArray()
                 res.send(result)
             })
-            app.get('/products/:id', async (req, res) => {
+            app.get('/products/:id',verifyJTW, async (req, res) => {
                 const id=req.params.id
                 const quary = {_id:ObjectId(id)}
                 const result = await productCollection.findOne(quary)
                 res.send(result)
             })
             
-            app.get('/reviews',async (req, res) => {
+            app.get('/reviews',verifyJTW,async (req, res) => {
                 const quary = {}
                 const result = await reviewsCollection.find(quary).toArray()
                 res.send(result)
             })
-            app.get('/myorder', async (req, res) => {
+            app.get('/myorder',verifyJTW, async (req, res) => {
                 const email = req.query.email;
-                console.log(email)
+           
                 const queary = { email }
                 const result= await orderCollection.find(queary).toArray()
                 res.send(result)
@@ -49,9 +71,8 @@ const run=async()=>{
 
 
 
-            app.post('/order', async (req, res) => {
+            app.post('/order',verifyJTW, async (req, res) => {
                 const order = req.body;
-                console.log(order.productname)
                 const quary = { productname:order.productname}
                 const exists = await orderCollection.findOne(quary)
                 if (exists) {
@@ -61,6 +82,30 @@ const run=async()=>{
                 res.send(result)
             })
 
+            app.put('/user/:email', async (req, res) => {
+                const email = req.params.email;
+                const user = req.body;
+                const filter = { email: email };
+                const options = { upsert: true };
+                const updateDoc = {
+                    $set: user,
+                };
+                const result = await userCollection.updateOne(filter, updateDoc, options)
+                  const token=jwt.sign({email:email},process.env.ACCES_TOKEN,{expiresIn:"1h"})
+                res.send({result,token})
+           })
+
+
+
+
+
+
+            app.delete('/orderdeleted/:id',verifyJTW, async (req, res) => {
+                const id = req.params.id;
+                const quray = { _id: ObjectId(id) }
+                const result = await orderCollection.deleteOne(quray)
+               res.send(result)
+            })
 
         }
         finally{
