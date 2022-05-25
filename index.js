1da3777f8c2ebd60e5e0ed3e540318835a542ef2
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-05-25 22:15:53 +0600

    jwt done

--- index.js
+++ index.js
@@ -9,9 +9,28 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 
+ const verifyJWT=(req, res, next)=> {
+     const authorization = req.headers.authorization
+     if (!authorization) {
+         return res.status(401).send(' UnAthorized Access')
+     }
+     const token = authorization.split(' ')[1]
+     jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+         if (err) {
+             res.status(403).send({message:"Fobidden Access"})
+         }
+         req.decoded = decoded
+            next()
+     })
+  
+ }
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwubk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 
 const run=async()=>{
         try{
@@ -26,7 +45,7 @@ const run=async()=>{
                 const result = await productCollection.find(quary).toArray()
                 res.send(result)
             })
-            app.get('/products/:id', async (req, res) => {
+            app.get('/products/:id',verifyJWT, async (req, res) => {
                 const id = req.params.id
                 const quary = {_id:ObjectId(id)}
                 const result = await productCollection.findOne(quary)
@@ -38,18 +57,22 @@ const run=async()=>{
                 const result = await reviewsCollection.find(quary).toArray()
                 res.send(result)
             })
-            app.get('/alluser',async (req, res) => {
+            app.get('/alluser',verifyJWT,async (req, res) => {
                 const quary = {}
                 const result = await userCollection.find(quary).toArray()
                 res.send(result)
             })
-            app.get('/myorder', async (req, res) => {
-                const email = req.query.email;
-                const authorization = req.headers.authorization
-                console.log(authorization)
-                const queary = { email }
+            app.get('/myorder',verifyJWT, async (req, res) => {
+                const email = req.query.email;    
+                const decodedEmail = req.decoded.email
+                if (decodedEmail === email) {
+                  const queary = { email }
                 const result= await orderCollection.find(queary).toArray()
                 res.send(result)
+                }
+                else {
+                    res.status(403).send({ message: "forbidden access" });
+               }
                
             })
 
@@ -58,7 +81,7 @@ const run=async()=>{
 
 
 
-            app.post('/order', async (req, res) => {
+            app.post('/order',verifyJWT, async (req, res) => {
                 const order = req.body;
                 const quary = { productname:order.email}
                 const exists = await orderCollection.findOne(quary)
@@ -84,7 +107,7 @@ const run=async()=>{
 
 
 
-            app.put('/user/admin/:email', async (req, res) => {
+            app.put('/user/admin/:email',verifyJWT, async (req, res) => {
                 const email = req.params.email;;
                 console.log(email)
                 const filter = { email: email };
@@ -100,7 +123,7 @@ const run=async()=>{
 
 
 
-            app.delete('/orderdeleted/:id', async (req, res) => {
+            app.delete('/orderdeleted/:id',verifyJWT, async (req, res) => {
                 const id = req.params.id;
                 const quray = { _id: ObjectId(id) }
                 const result = await orderCollection.deleteOne(quray)
