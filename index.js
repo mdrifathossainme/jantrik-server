59c8dc1e2474e8083375b533521499e60980e80d
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-05-25 20:00:59 +0600

    make admin

--- index.js
+++ index.js
@@ -9,24 +9,6 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 
-const varyfyJTW=(req,res,next)=>{
-const authoHeader=req.headers.authorization;
-if(!authoHeader){
-    return res.status(401).send({massage:"you are UnAuthorized access"})
-
-}
-const token= authoHeader.split(' ')[1]
-jwt.verify(token,process.env.ACCESS_TOKEN,function(err,decoded){
-    if(err){
-        return  res.status(401).send({message:"Forbidden access"})
-    }
-    req.decoded=decoded;
-    next()
-})
-}
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwubk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -45,7 +27,9 @@ const run=async()=>{
                 res.send(result)
             })
             app.get('/products/:id', async (req, res) => {
-                const id=req.params.id
+                const id = req.params.id
+              
+                
                 const quary = {_id:ObjectId(id)}
                 const result = await productCollection.findOne(quary)
                 res.send(result)
@@ -54,6 +38,11 @@ const run=async()=>{
             app.get('/reviews',async (req, res) => {
                 const quary = {}
                 const result = await reviewsCollection.find(quary).toArray()
+                res.send(result)
+            })
+            app.get('/alluser',async (req, res) => {
+                const quary = {}
+                const result = await userCollection.find(quary).toArray()
                 res.send(result)
             })
             app.get('/myorder', async (req, res) => {
@@ -72,7 +61,7 @@ const run=async()=>{
 
             app.post('/order', async (req, res) => {
                 const order = req.body;
-                const quary = { productname:order.productname}
+                const quary = { productname:order.email}
                 const exists = await orderCollection.findOne(quary)
                 if (exists) {
                     return res.send({success:false,product:exists})
@@ -90,8 +79,20 @@ const run=async()=>{
                     $set: user,
                 };
                 const result = await userCollection.updateOne(filter, updateDoc, options)
-                const token=jwt.sign({email:email}, process.env.ACCESS_TOKEN,{expiresIn:"2h"})
-                res.send({result,token})
+                res.send({result})
+            })
+
+
+
+            app.put('/user/admin/:email', async (req, res) => {
+                const email = req.params.email;;
+                console.log(email)
+                const filter = { email: email };
+                const updateDoc = {
+                    $set:{role:"admin"},
+                };
+                const result = await userCollection.updateOne(filter, updateDoc)
+                res.send(result)
            })
 
 
