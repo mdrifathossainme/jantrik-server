2d71fdca3214bdb725f68824943a400f1eb4dcc7
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-05-26 10:55:32 +0600

    admin handle done

--- index.js
+++ index.js
@@ -83,6 +83,17 @@ const run=async()=>{
                 res.send(result)    
             })
 
+            
+            app.get('/admin/:email', async(req,res)=>{
+                const email= req.params.email
+                const user= await userCollection.findOne({email:email})
+                const isAdmin=user.role==="admin"
+                res.send({admin:isAdmin})
+            })
+
+
+
+
           
 
             app.post('/order',verifyJWT, async (req, res) => {
