fd88888ff3168561e5c6105513d59e247e4660a8
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-05-26 23:56:02 +0600

    end

--- index.js
+++ index.js
@@ -30,8 +30,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-
-
 const run=async()=>{
         try{
             await client.connect();
