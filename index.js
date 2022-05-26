7084f406642d5721157a71bb5316f281a0b27d13
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-05-26 07:58:14 +0600

    error

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.use(cors())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwubk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri)
 
 
 
